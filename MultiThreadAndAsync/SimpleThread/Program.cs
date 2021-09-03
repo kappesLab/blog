c55ae3962a44526cfe3ee96e216df5e30e6cdc66
Davide Cappelli (ASUS) <davide.kappelli@gmail.com>
2021-09-03 11:46:08 +0200

    First commit

---
+++ MultiThreadAndAsync/SimpleThread/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th = new Thread(SayHello);
+            th.Name = "Hallo";
+            th.Priority = ThreadPriority.Normal;
+            th.Start();
+
+            // Press RETURN to exit
+            Console.ReadLine();
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine("Hello Word");
+        }
+
+    }
+}
