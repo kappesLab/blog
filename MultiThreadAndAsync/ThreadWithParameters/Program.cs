930dbd843038a12872309a91a1eb46b2cc26128d
Davide Cappelli (ASUS) <davide.kappelli@gmail.com>
2021-09-04 11:29:47 +0200

    Add project "ThreadWithParameters"

---
+++ MultiThreadAndAsync/ThreadWithParameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadWith_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th = new Thread(SayHello);
+            th.Name = "Hello";
+            th.Priority = ThreadPriority.Normal;
+            th.Start("Davide");
+
+            // Press RETURN to exit
+            Console.ReadLine();
+        }
+
+        private static void SayHello(object o)
+        {
+            Console.WriteLine($"Hello World {o}");
+        }
+    }
+}
