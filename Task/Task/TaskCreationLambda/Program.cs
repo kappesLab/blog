10243b06829d726d6aa72bf6959314c8c1329aa7
Davide Cappelli <davide.kappelli@hotmail.it>
2021-09-17 20:09:02 +0200

    Add Task Creation Project

--- Task/Task/TaskCreationLambda/Program.cs
+++ Task/Task/TaskCreationLambda/Program.cs
@@ -5,26 +5,24 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace TaskCreation
+namespace TaskCreationLambda
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Task task = new Task(DoStuff);
+            Task task = new Task(() =>
+            {
+                // Simulates a long-term operation
+                Thread.Sleep(500);
+                Console.WriteLine("Ciao mondo! Io sono il lavoro del task.");
+            });
             task.Start();
 
             Console.WriteLine("Io sono nel metodo chiamante.");
 
             // Press ENTER to exit
             Console.ReadLine();
-        }
-
-        private static void DoStuff()
-        {
-            // Simulates a long-term operation
-            Thread.Sleep(500);
-            Console.WriteLine("Ciao mondo! Io sono il lavoro del task.");            
         }
     }
 }
