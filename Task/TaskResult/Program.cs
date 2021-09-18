c2aa19b82f5047f9ef7e4934b39522d86b589bf6
Davide Cappelli <davide.kappelli@hotmail.it>
2021-09-18 17:07:39 +0200

    Add project Task Result

---
+++ Task/TaskResult/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> output = Task.Factory.StartNew(DoStuffWithReturn);
+
+            Console.WriteLine("Sono nel metodo chiamante PRIMA della lettura del risultato.");
+            Console.WriteLine($"La task ha restituito: {output.Result}.");
+
+            // Press ENTER to exit
+            Console.ReadLine();
+        }
+
+        private static int DoStuffWithReturn()
+        {
+            // Simulates a long-term operation
+            Thread.Sleep(1000);
+            Console.WriteLine("Io sono DoStuffWithReturn.");
+            return 1001;
+        }
+    }
+}
