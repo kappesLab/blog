e1967936ee753dd295bc11d83fbcab242e78a5a6
Davide Cappelli <davide.kappelli@hotmail.it>
2021-09-18 17:38:28 +0200

    Add project TaskParameter

---
+++ Task/TaskParameter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(DoStuff, i);
+            }
+
+            // Press ENTER to exit
+            Console.ReadLine();
+        }
+
+
+        private static void DoStuff(object num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
