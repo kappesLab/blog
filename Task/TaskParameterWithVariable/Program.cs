6a27120e3c53e0f7a356bd0ecaeb10d06d46d9cb
Davide Cappelli <davide.kappelli@hotmail.it>
2021-09-18 17:28:34 +0200

    Add project TaskParameterWithvariable

---
+++ Task/TaskParameterWithVariable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskParameterWithVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine(i);
+                });
+            }
+
+            // Press ENTER to exit
+            Console.ReadLine();
+        }
+    }
+}
