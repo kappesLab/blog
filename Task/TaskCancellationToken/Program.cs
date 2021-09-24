3c4701d1b729bca59137296aa0c8d1b2bce355a6
Davide Cappelli (ASUS) <davide.kappelli@gmail.com>
2021-09-24 16:33:06 +0200

    Add Task Projects

---
+++ Task/TaskCancellationToken/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCancellationToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokenSource = new CancellationTokenSource();
+
+            // Pass the same token source to the delegate and to the task instance.
+            Task output = Task.Run(() => DoStuff(tokenSource.Token), tokenSource.Token);
+
+            Console.WriteLine("Premi un tasto per interrompere il task.");
+            
+            Console.ReadKey();
+            tokenSource.Cancel();
+
+            output.Wait();
+            Console.WriteLine($"output.Status = {output.Status}");
+
+            // Press ENTER to exit
+            Console.ReadLine();
+        }
+
+
+        private static void DoStuff(CancellationToken token)
+        {
+            // Simulates a long-term operation
+            while (true)
+            {
+                Console.WriteLine("Io sono il lavoro del task.");
+                Thread.Sleep(500);
+
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Task/TaskCancellationTokenException/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace TaskCancellationTokenException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokenSource = new CancellationTokenSource();
+
+            // Pass the same token source to the delegate and to the task instance.
+            Task output = Task.Run(() => DoStuff(tokenSource.Token), tokenSource.Token);
+
+            Console.WriteLine("Premi un tasto per interrompere il task.");
+
+            Console.ReadKey();
+            tokenSource.Cancel();
+            try
+            {
+                output.Wait();
+                Console.WriteLine($"output.Status = {output.Status}");
+            }
+            catch (AggregateException aggEx)
+            {
+                foreach (var e in aggEx.InnerExceptions)
+                {
+                    // Handle the custom exception.
+                    if (e is OperationCanceledException)
+                    {
+                        Console.WriteLine($"Task Cancellata: {e.Message}");
+                    }
+                }
+                   
+                Console.WriteLine($"output.Status = {output.Status}");
+            }
+            finally
+            {
+                tokenSource.Dispose();
+            }
+          
+
+            // Press ENTER to exit
+            Console.ReadLine();
+        }
+
+
+
+        private static void DoStuff(CancellationToken token)
+        {
+            // Simulates a long-term operation
+            while (true)
+            {
+                Console.WriteLine("Io sono il lavoro del task.");
+                Thread.Sleep(500);
+
+                token.ThrowIfCancellationRequested();
+            }
+        }
+
+
+    }
+}
