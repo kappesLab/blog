69efd54dba51892c77eefdea4b9da04fbd67464d
Davide Cappelli <davide.kappelli@hotmail.it>
2021-10-16 12:09:28 +0200

    Add project MulticastDelegate

---
+++ Delegates/MulticastDelegate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegate
+{
+    class Program
+    {
+        // delegate type declaration
+        public delegate void MyDelegate(string s);
+        static void Main(string[] args)
+        {
+            MyDelegate handler = SayHello;
+            handler += Tanks;
+
+            handler("Davide");
+
+            Console.WriteLine(" ----------------- ");
+
+            handler -= Tanks;
+            handler += Scold;
+
+            handler("Marco");
+
+            // Press ENTER to exit
+            Console.ReadLine();
+        }
+
+        public static void SayHello(string name)
+        {
+            Console.WriteLine($"Ciao, {name}");
+        }
+
+        public static void Tanks(string name)
+        {
+            Console.WriteLine($"Grazie, {name}");
+        }
+
+        public static void Scold(string name)
+        {
+            Console.WriteLine($"Cosi non va bene, {name}!");
+        }
+    }
+}
