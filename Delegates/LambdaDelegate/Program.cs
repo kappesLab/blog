eebbf15b099d1798a213ebfd4b071c42593b7941
Davide Cappelli <davide.kappelli@hotmail.it>
2021-10-16 09:58:01 +0200

    Add LambdaDelegate project

---
+++ Delegates/LambdaDelegate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaDelegate
+{
+    class Program
+    {
+        // delegate type declaration
+        public delegate bool PerformComparison(int x, string s);
+
+        static void Main(string[] args)
+        {
+            PerformComparison handler = (int n, string t) => t.Length > n;
+          
+            Console.WriteLine($"{handler(6, "PIPPO")} = PerformComparison(6, 'PIPPO')");
+
+            Console.WriteLine($"{handler(6, "PAPERINO")} = PerformComparison(6, 'PAPERINO')");
+
+            // Press ENTER to exit
+            Console.ReadLine();
+        }
+    }
+}
