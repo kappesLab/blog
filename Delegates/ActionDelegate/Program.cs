eb18bf5985f1b193bb3e48af13b65aaae4dd4ba1
Davide Cappelli <davide.kappelli@hotmail.it>
2021-10-17 10:02:27 +0200

    Add Action Project

---
+++ Delegates/ActionDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action handler = SayHello;
+            handler();
+
+            // Press ENTER to exit
+            Console.ReadLine();
+        }
+
+        public static void SayHello()
+        {
+            Console.WriteLine($"Ciao");
+        }
+
+        public static void Tanks()
+        {
+            Console.WriteLine($"Grazie");
+        }
+
+    }
+}
